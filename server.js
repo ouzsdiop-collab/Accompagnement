6cc849c3a3ecf1da954c03065903cacc77bb28bd
ouzsdiop-collab <ouzsdiop@gmail.com>
2026-05-12 14:33:57 +0200

    Prepare static app for Railway deployment

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`QHSE Control candidature app running on port ${PORT}`);
+});
